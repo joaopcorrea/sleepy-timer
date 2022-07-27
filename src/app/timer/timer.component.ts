58fcdc62b801b527b2d62d0ea61d0d121e05ee73
João Pedro Corrêa <joaopedrocorrea21@gmail.com>
2022-07-26 23:10:50 -0300

    feat: creating the timer

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -7,8 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimerComponent implements OnInit {
 
-  time: number = 0;
   timer: any;
+
+  hours: number = 0;
+  minutes: number = 0;
+  seconds: number = 0;
+
+  private lastHours: number = 0;
+  private lastMinutes: number = 0;
+  private lastSeconds: number = 0;
+
+  isRunning: boolean = false;
 
   constructor() { }
 
@@ -16,17 +25,119 @@ export class TimerComponent implements OnInit {
     
   }
 
-  handleClickStart() {
-    if (this.timer) return;
-    
+  handleClickMore(type: string) {
+    if (this.isRunning) return;
+
+    switch (type) {
+      case 'H':
+        this.hours++;
+        break;
+        
+        case 'M':
+          if (this.minutes >= 59) return;
+          this.minutes++;
+        break;
+
+      case 'S':
+        if (this.seconds >= 59) return;
+        this.seconds++;
+        break;
+    }
+  }
+
+  handleClickLess(type: string) {
+    if (this.isRunning) return;
+
+    switch (type) {
+      case 'H':
+        if (!this.hours) return;
+        this.hours--;
+        break;
+
+      case 'M':
+        if (!this.minutes) return;
+        this.minutes--;
+        break;
+
+      case 'S':
+        if (!this.seconds) return;
+        this.seconds--;
+        break;
+    }
+  }
+
+  updateTimer() {
+    console.log('updating');
+
+    if (!this.seconds) {
+      if (!this.minutes) {
+        if (!this.hours) {
+          this.endTimer();
+          return;
+        }
+
+        console.log('menos hora');
+        this.hours--;
+        this.minutes = 59;
+        this.seconds = 59;
+      }
+      else {
+        console.log('menos minuto');
+        this.minutes--;
+        this.seconds = 59;
+      }
+    }
+    else
+    {
+      this.seconds--;
+    }
+  }
+
+  endTimer() {
+    console.log('end timer');
+    clearInterval(this.timer);
+    this.timer = null;
+    this.isRunning = false;
+  }
+
+  handleClickPlayPause() {
+    if (this.isRunning) 
+      this.PauseTimer();
+    else 
+      this.PlayTimer();
+  }
+
+  PlayTimer() {
+    this.isRunning = true;
+
+    this.lastHours = this.hours;
+    this.lastMinutes = this.minutes;
+    this.lastSeconds = this.seconds;
+
     this.timer = setInterval(() => {
-      this.time++;
+      this.updateTimer();
     }, 1000);
   }
 
+  PauseTimer() {
+    this.isRunning = false;
+    this.endTimer();
+  }
+
+  handleClickReset() {
+    this.PauseTimer();
+
+    this.hours = this.lastHours;
+    this.minutes = this.lastMinutes;
+    this.seconds = this.lastSeconds;
+  }
+
   handleClickStop() {
-    clearInterval(this.timer);
-    this.timer = null;
+    this.endTimer();
+
+    this.hours = 0;
+    this.minutes = 0;
+    this.seconds = 0;
   }
 
 }
