3902d59ab6019b2f07d5bb3691d9999a28957f65
João Pedro Corrêa <joaopedrocorrea21@gmail.com>
2022-07-27 19:03:27 -0300

    🚧 feat: adding electron

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 import { TimerComponent } from './timer/timer.component';
 
 @NgModule({
@@ -10,7 +11,8 @@ import { TimerComponent } from './timer/timer.component';
     TimerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
