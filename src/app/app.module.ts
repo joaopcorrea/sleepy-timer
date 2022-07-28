05b2846f31da342903b7f525bb7428a6d78615fd
João Pedro Corrêa <joaopedrocorrea21@gmail.com>
2022-07-27 21:40:31 -0300

    🚧 feat: added electron

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
@@ -12,6 +14,7 @@ import { TimerComponent } from './timer/timer.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     CoreModule
   ],
   providers: [],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,9 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
+export const APP_CONFIG = {
+  production: false,
+  environment: 'LOCAL'
 };
 
 /*
